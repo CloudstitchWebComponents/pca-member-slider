4592f7415d1bfd5aba3f062c77ea55333e09aab8
Ted Benson <ted@cloudstitch.io>
2016-01-31 23:21:51 -0800

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// Include Gulp & Tools We'll Use
+var gulp = require('gulp');
+var bower_package = require('./bower.json');
+
+require('../utils/gulp-master')(gulp, bower_package);
+
